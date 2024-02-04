cd59ea91b871cf7922e1b668b0d56d70a8f797bb
nadeen-maree <nadeen.maree1610@gmail.com>
2024-02-04 17:40:35 +0200

    Local Storage Extension Added

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,57 +1,89 @@
-import React, { useState } from 'react';
-import Movie from './Movie'; 
+import React, { useState, useEffect } from 'react';
+import Movie from './Movie';
 
 const Catalog = ({ users, movies, setMovies }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [rentedMovies, setRentedMovies] = useState([]);
   const [budget, setBudget] = useState(10);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const userId = localStorage.getItem('currentUserId');
+    const storedUserData = localStorage.getItem(userId);
+    const userData = storedUserData ? JSON.parse(storedUserData) : {};
+
+    setCurrentUser(users.find(user => user.id === Number(userId)));
+    setRentedMovies(userData.rentedMovies || []);
+    setBudget(userData.budget || 10);
+  }, [users]);
 
   const rentMovie = (movie) => {
     if (budget < 3) {
-        alert('Insufficient funds. Cannot rent the movie.');
-    } else
-    if (!movie.isRented) {
-      setRentedMovies([...rentedMovies, movie]);
+      alert('Insufficient funds. Cannot rent the movie.');
+    } else if (!movie.isRented) {
+      const updatedRentedMovies = [...rentedMovies, movie];
+      setRentedMovies(updatedRentedMovies);
       setMovies((prevMovies) =>
         prevMovies.map((m) => (m.id === movie.id ? { ...m, isRented: true } : m))
       );
       setBudget(budget - 3);
+
+      updateLocalStorage(updatedRentedMovies);
     }
   };
 
   const unRentMovie = (movie) => {
-    setRentedMovies(rentedMovies.filter((m) => m.id !== movie.id));
+    const updatedRentedMovies = rentedMovies.filter((m) => m.id !== movie.id);
+    setRentedMovies(updatedRentedMovies);
     setMovies((prevMovies) =>
       prevMovies.map((m) => (m.id === movie.id ? { ...m, isRented: false } : m))
     );
     setBudget(budget + 3);
+
+    updateLocalStorage(updatedRentedMovies);
   };
 
+  const updateLocalStorage = (rentedMovies) => {
+    const userId = localStorage.getItem('currentUserId');
+    localStorage.setItem(
+      userId,
+      JSON.stringify({
+        rentedMovies: rentedMovies,
+        budget: budget,
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (currentUser) {
+      updateLocalStorage(rentedMovies);
+    }
+  }, [rentedMovies, budget, currentUser]);
 
   const filterMovies = () => {
     if (!searchQuery) {
       return movies;
     }
-  
+
     const lowercasedQuery = searchQuery.toLowerCase();
-  
+
     return movies.filter((movie) => {
       const lowercasedTitle = movie.title.toLowerCase();
       return lowercasedTitle.includes(lowercasedQuery);
     });
   };
-  
 
   const filteredMovies = filterMovies();
 
   return (
     <div className='catalog'>
+      {currentUser && <h1 className='welcome' style={{ color: currentUser.backgroundColor }}>Welcome Back, {currentUser.name}!</h1>}
       <h3 className='budget'>Budget: {budget}$</h3>
 
       <div className='search-bar'>
         <input
-          type="text"
-          placeholder="Search movies..."
+          type='text'
+          placeholder='Search movies...'
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
@@ -70,16 +102,15 @@ const Catalog = ({ users, movies, setMovies }) => {
         </div>
       )}
       <h1 className='header-title'>Catalog</h1>
-      {filteredMovies
-        .map((movie) => (
-          <Movie
-            key={movie.id}
-            movie={movie}
-            onRent={rentMovie}
-            onUnRent={unRentMovie}
-            isRented={rentedMovies.some((rentedMovie) => rentedMovie.id === movie.id)}
-          />
-        ))}
+      {filteredMovies.map((movie) => (
+        <Movie
+          key={movie.id}
+          movie={movie}
+          onRent={rentMovie}
+          onUnRent={unRentMovie}
+          isRented={rentedMovies.some((rentedMovie) => rentedMovie.id === movie.id)}
+        />
+      ))}
     </div>
   );
 };
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Landing = ({users}) => {
+const Landing = ({ users }) => {
+  const handleUserClick = (user) => {
+    localStorage.setItem('currentUserId', user.id);
+    localStorage.setItem('currentUserData', JSON.stringify(user));
+  };
 
   return (
     <div>
       <h4 className='landing-question'>WHO'S WATCHING?</h4>
       {users.map((user) => (
-       <Link key={user.id} to="/catalog">
-            <div
-                className='user-template'
-                style={{ backgroundColor: user.backgroundColor }}
-            >
-                {user.name}
-            </div>
+        <Link key={user.id} to="/catalog" onClick={() => handleUserClick(user)}>
+          <div
+            className='user-template'
+            style={{ backgroundColor: user.backgroundColor }}
+          >
+            {user.name}
+          </div>
         </Link>
       ))}
     </div>
